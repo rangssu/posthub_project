486caece9dfe1faf15d7b61189ade089188aa29f
rangssu <wlqkrtls6042@gmail.com>
2025-12-29 12:33:36 +0900

    Inital project setup

---
+++ src/main/java/com/posthub/posthub/PosthubApplication.java
@@ -0,0 +1,13 @@
+package com.posthub.posthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosthubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PosthubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/posthub/posthub/PosthubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.posthub.posthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosthubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
